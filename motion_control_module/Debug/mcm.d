25bf8ba2665bf5961c006e5eed5ffd2ddb32665e
nlman0 <nlandy8@gmail.com>
2018-04-01 21:54:51 -0400

    Uploaded MCM Code

---
+++ motion_control_module/Debug/mcm.d
@@ -0,0 +1,87 @@
+# FIXED
+
+mcm.obj: ../mcm.c
+mcm.obj: ../driverlib.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/adc14.h
+mcm.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+mcm.obj: C:/ti/ccsv7/ccs_base/arm/include/msp.h
+mcm.obj: C:/ti/ccsv7/ccs_base/arm/include/msp432p401r.h
+mcm.obj: C:/ti/ccsv7/ccs_base/arm/include/msp_compatibility.h
+mcm.obj: C:/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+mcm.obj: C:/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h
+mcm.obj: C:/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h
+mcm.obj: C:/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+mcm.obj: C:/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h
+mcm.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/aes256.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/comp_e.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/cpu.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/crc32.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/cs.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/dma.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/eusci.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/flash.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/fpu.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/gpio.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/i2c.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/interrupt.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/mpu.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/pcm.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/pmap.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/pss.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/ref_a.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/reset.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/rom.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/rom_map.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/rtc_c.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/spi.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/sysctl.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/systick.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/timer32.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/timer_a.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/uart.h
+mcm.obj: C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/wdt_a.h
+mcm.obj: ../ClockSys.h
+
+../mcm.c: 
+../driverlib.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/adc14.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+C:/ti/ccsv7/ccs_base/arm/include/msp.h: 
+C:/ti/ccsv7/ccs_base/arm/include/msp432p401r.h: 
+C:/ti/ccsv7/ccs_base/arm/include/msp_compatibility.h: 
+C:/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+C:/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/aes256.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/comp_e.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/cpu.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/crc32.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/cs.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/dma.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/eusci.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/flash.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/fpu.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/gpio.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/i2c.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/interrupt.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/mpu.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/pcm.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/pmap.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/pss.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/ref_a.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/reset.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/rom.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/rom_map.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/rtc_c.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/spi.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/sysctl.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/systick.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/timer32.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/timer_a.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/uart.h: 
+C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/wdt_a.h: 
+../ClockSys.h: 
---
+++ motion_control_module/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"C:/Users/nlandy/Documents/School/Fa17/uP2/Backup-UF-EEL4930-Students/Libraries/BoardSupportPackage/DriverLib/msp432p4xx_driverlib.lib"
+

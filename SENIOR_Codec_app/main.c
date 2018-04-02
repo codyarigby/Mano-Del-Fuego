b3dfdef2dee3121c7547fdaa2f85e80c062c589c
CODYLAPTOP\Cody <codyarigby@gmail.com>
2018-04-01 22:08:43 -0400

    latest commit with spi initialization and struct initialization

--- SENIOR_Codec_app/main.c
+++ SENIOR_Codec_app/main.c
@@ -136,9 +136,12 @@ int sci_receive(void);  // uart receive function
 void init_dma(void);
 void init_mcbspa(void);
 void init_Xintf(void);
-
-
-
+static void LCD_initSPI(void);
+void spi_xmit(Uint16 a);
+void button_1(void);
+void button_2(void);
+void button_3(void);
+void button_4(void);
 
 interrupt void local_D_INTCH1_ISR(void); 	// Channel 1 Rx ISR
 interrupt void local_D_INTCH2_ISR(void); 	// Channel 2 Tx ISR
@@ -315,15 +318,15 @@ Uint16 ext_Buffer_size = 32767; // and these pointers with 0x7FFF
 // *******************************************************************************************************
 
 struct PITCH_SHIFT{
-Uint16 Direction;
-float PerUpper;
-float PerLower;
-float Per;
-Uint16 count_delay1;
-Uint16 ext_index_delay1;
-int16 ext_delay;
-int16 ext_delay_prev;
-Uint16 PerReset;
+	Uint16 Direction;
+	float PerUpper;
+	float PerLower;
+	float Per;
+	Uint16 count_delay1;
+	Uint16 ext_index_delay1;
+	int16 ext_delay;
+	int16 ext_delay_prev;
+	Uint16 PerReset;
 } pitch_shift;
 
 
@@ -497,6 +500,19 @@ float *dptr = &dummy_buffer[0];
 
 
 // *******************************************************************************************************
+// 							UI Structures
+// *******************************************************************************************************
+struct EFFECT_SEL
+{
+	Uint16 effect_selection;  // the effect that was selected (index of Effect Chain)
+}effect_sel;
+
+struct MENU_SEL
+{
+	Uint16 current;
+}menu;
+
+// *******************************************************************************************************
 // 							Effects Structure
 // *******************************************************************************************************
 struct EFFECT_CHAIN
@@ -529,6 +545,7 @@ void main(void)
    InitMcbspGpio();
    init_uart();
    init_Xintf();
+   LCD_initSPI();
 
    // Fill the buffers with dummy data
    for(k=0; k<p_buff_size; k++) { ping_buffer[k] = 0x0000; }
@@ -549,6 +566,8 @@ void main(void)
 
    EALLOW;
    DINT;						// Disable interrupts again (for now)
+
+
 
    // Disable CPU interrupts and clear all CPU interrupt flags:
    IER = 0x0000;
@@ -598,6 +617,7 @@ void main(void)
 	PieCtrlRegs.PIEIER9.bit.INTx3 = 1;  // Enable INTx.3 of INT9 (SCIB RXRDY Flag)
 
 	//DmaRegs.CH1.CONTROL.bit.RUN = 1; // Start rx on Channel 1
+
 
 // *******************************************************************************************************
 // 							Initialize the Bluetooth Module
@@ -1691,6 +1711,223 @@ void init_Xintf(void)
 	 EDIS;
 
 }
+
+static void LCD_initSPI(void)
+{
+
+    // Step 2. Initialize GPIO:
+    // This example function is found in the DSP2833x_Gpio.c file and
+    // illustrates how to set the GPIO to it's default state.
+    // InitGpio();  // Skipped for this example
+    // Setup only the GP I/O only for SPI-A functionality
+    // This function is found in DSP2833x_Spi.c
+    EALLOW;
+ /* Enable internal pull-up for the selected pins */
+ // Pull-ups can be enabled or disabled by the user.
+ // This will enable the pullups for the specified pins.
+ // Comment out other unwanted lines.
+
+ //  GpioCtrlRegs.GPAPUD.bit.GPIO16 = 0;   // Enable pull-up on GPIO16 (SPISIMOA)
+ //  GpioCtrlRegs.GPAPUD.bit.GPIO17 = 0;   // Enable pull-up on GPIO17 (SPISOMIA)
+ //  GpioCtrlRegs.GPAPUD.bit.GPIO18 = 0;   // Enable pull-up on GPIO18 (SPICLKA)
+ //  GpioCtrlRegs.GPAPUD.bit.GPIO19 = 0;   // Enable pull-up on GPIO19 (SPISTEA)
+
+
+     GpioCtrlRegs.GPBPUD.bit.GPIO54 = 0;   // Enable pull-up on GPIO54 (SPISIMOA)
+     GpioCtrlRegs.GPBPUD.bit.GPIO55 = 0;   // Enable pull-up on GPIO55 (SPISOMIA)
+     GpioCtrlRegs.GPBPUD.bit.GPIO56 = 0;   // Enable pull-up on GPIO56 (SPICLKA)
+     GpioCtrlRegs.GPBPUD.bit.GPIO57 = 0;   // Enable pull-up on GPIO57 (SPISTEA)
+
+ /* Set qualification for selected pins to asynch only */
+ // This will select asynch (no qualification) for the selected pins.
+ // Comment out other unwanted lines.
+
+ //    GpioCtrlRegs.GPAQSEL2.bit.GPIO16 = 3; // Asynch input GPIO16 (SPISIMOA)
+ //    GpioCtrlRegs.GPAQSEL2.bit.GPIO17 = 3; // Asynch input GPIO17 (SPISOMIA)
+ //    GpioCtrlRegs.GPAQSEL2.bit.GPIO18 = 3; // Asynch input GPIO18 (SPICLKA)
+     //GpioCtrlRegs.GPAQSEL2.bit.GPIO19 = 3; // Asynch input GPIO19 (SPISTEA)
+
+     GpioCtrlRegs.GPBQSEL2.bit.GPIO54 = 3; // Asynch input GPIO16 (SPISIMOA)
+     GpioCtrlRegs.GPBQSEL2.bit.GPIO55 = 3; // Asynch input GPIO17 (SPISOMIA)
+     GpioCtrlRegs.GPBQSEL2.bit.GPIO56 = 3; // Asynch input GPIO18 (SPICLKA)
+     GpioCtrlRegs.GPBQSEL2.bit.GPIO57 = 3; // Asynch input GPIO19 (SPISTEA)
+
+
+ /* Configure SPI-A pins using GPIO regs*/
+ // This specifies which of the possible GPIO pins will be SPI functional pins.
+ // Comment out other unwanted lines.
+
+ //     GpioCtrlRegs.GPAMUX2.bit.GPIO16 = 1; // Configure GPIO16 as SPISIMOA
+ //    GpioCtrlRegs.GPAMUX2.bit.GPIO17 = 1; // Configure GPIO17 as SPISOMIA
+ //    GpioCtrlRegs.GPAMUX2.bit.GPIO18 = 1; // Configure GPIO18 as SPICLKA
+     //GpioCtrlRegs.GPAMUX2.bit.GPIO19 = 1; // Configure GPIO19 as SPISTEA
+
+     GpioCtrlRegs.GPBMUX2.bit.GPIO54 = 1; // Configure GPIO54 as SPISIMOA
+     GpioCtrlRegs.GPBMUX2.bit.GPIO55 = 1; // Configure GPIO55 as SPISOMIA
+     GpioCtrlRegs.GPBMUX2.bit.GPIO56 = 1; // Configure GPIO56 as SPICLKA
+     GpioCtrlRegs.GPBMUX2.bit.GPIO57 = 1; // Configure GPIO57 as SPISTEA
+
+     //EDIS;
+
+    //GpioCtrlRegs.GPBMUX2.bit.GPIO57 = 0; // Configure GPIO19 as GPIO
+    //GpioCtrlRegs.GPBDIR.bit.GPIO57 = 1; //Configure GPIO19 as output
+    //GpioCtrlRegs.GPBPUD.bit.GPIO57 = 1;   // Disable pull-up on GPIO19 (SPISTEA)
+    //SPI_CS_LCD_HIGH;
+
+
+    // Initialize SPI FIFO registers
+    SpiaRegs.SPIFFTX.all=0xE040;
+    SpiaRegs.SPIFFRX.all=0x204f;
+    SpiaRegs.SPIFFCT.all=0x0;
+
+    SpiaRegs.SPICCR.all =0x000F;                 // Reset on, rising edge, 16-bit char bits
+    SpiaRegs.SPICTL.all =0x0006;                 // Enable master mode, normal phase,
+                                                 // enable talk, and SPI int disabled.
+    SpiaRegs.SPIBRR =0x0003;
+    SpiaRegs.SPICCR.all =0x00D7;                 // Relinquish SPI from Reset
+    SpiaRegs.SPIPRI.bit.FREE = 1;                // Set so breakpoints don't disturb xmission
+    EDIS;
+
+}
+
+//send a spi transfer
+void spi_xmit(Uint16 a)
+{
+    SpiaRegs.SPITXBUF=a;
+}
+
+//initialize all structures
+void init_structs(void)
+{
+
+	/*
+	struct FLANG {
+		Uint16 upper_delay;
+		Uint16 lower_delay;
+		Uint16 speed;
+		Uint16 delay;
+		int16  delay_index;
+	} flang;
+	*/
+
+	flang.upper_delay = 1024;
+	flang.lower_delay = 256;
+	flang.speed = 100;    		// 100us default
+	flang.delay = (flang.upper_delay - flang.lower_delay)>>1 + flang.lower_delay;
+
+
+/*
+	struct DELAY {
+		Uint16 upper_delay;
+		Uint16 lower_delay;
+		Uint16 speed;
+		Uint16 delay;
+		int16  delay_index;
+	} dig_delay;
+*/
+
+
+/*
+	struct TREMOLO {
+		Uint16 amplitude;
+		Uint16 period;
+	} tremolo;
+*/
+
+
+/*
+	struct SVF {
+	   float Fupper;		// upper boundary of F
+	   float Flower;        // lower boundary of F
+	   float F;				// Tuning Coefficient  F1 = 2sin(pi*fc/fs)
+	   float Q;				// Tuning Coefficient  Q1 = 2sigma
+	   float  HPF;
+	   float  BPF[2];
+	   float  LPF[2];
+	};
+	struct SVF wah_svf;
+	struct SVF bass_svf;
+	struct SVF treble_svf;
+*/
+
+
+
+/*
+	struct PITCH_SHIFT{
+		Uint16 Direction;
+		float PerUpper;
+		float PerLower;
+		float Per;
+		Uint16 count_delay1;
+		Uint16 ext_index_delay1;
+		int16 ext_delay;
+		int16 ext_delay_prev;
+		Uint16 PerReset;
+	} pitch_shift;
+*/
+
+
+
+/*
+	struct EFFECT_SEL
+	{
+		Uint16 effect_selection;  // the effect that was selected (index of Effect Chain)
+	}effect_sel;
+*/
+
+
+
+/*
+	struct MENU_SEL
+	{
+		Uint16 current;
+	}menu;
+*/
+
+
+/*
+	struct EFFECT_CHAIN
+	{
+		Uint16 effect    [4];	// effect number for effect define
+		Uint32 effect_ptr[4];	// Array of pointers to effects structures
+	};
+*/
+
+
+/*
+	struct SAMPLE {
+		   int16 samp[5];
+		   Uint16 index[5];
+		   Uint16 count;
+	} sample;
+*/
+
+
+/*
+	struct ZCROSS_POS {
+	   int16   cross_samp[5];
+	   Uint16  cross_index[5];
+	   Uint16  count_delay;
+	} zcross_pos;
+*/
+
+
+
+/*
+	struct ZCROSS_NEG{
+		   int16   cross_samp[5];
+		   Uint16  cross_index[5];
+		   Uint16  count_delay;
+	} zcross_neg;
+*/
+
+
+
+
+}
+
+
+
 
 
 
